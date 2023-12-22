e87ad1203a2ba714188ca37360e02c389ac1bf7d
MinhTri <str.comparetoignore@gmail.com>
2023-12-22 11:16:25 +0700

    moveforward

--- index.js
+++ index.js
@@ -1,39 +1,67 @@
-import { WebSocketServer } from "ws";
+import express from 'express';
+import { createServer } from 'http';
+import { WebSocketServer } from 'ws';
 
+// Khởi tạo Express app
+const app = express();
 const PORT = process.env.PORT || 3000;
-const wss = new WebSocketServer({ port: PORT });
+
+// Sử dụng body-parser để xử lý JSON từ body request
+app.use(express.json());
+
+// Lưu trữ client WebSocket
+let connectedWs = null;
+
+// Tạo WebSocket server
+const wss = new WebSocketServer({ noServer: true });
 
 wss.on('connection', (ws) => {
-    console.log("Client Connected");
-
-    // Đánh dấu client là 'alive' khi kết nối
-    ws.isAlive = true;
+    console.log("WebSocket client connected");
+    connectedWs = ws;
 
     ws.on('pong', () => {
-        // Đánh dấu lại client là 'alive' khi nhận được pong
         ws.isAlive = true;
     });
 
     ws.on('message', (message) => {
-        console.log("Received: ", message);
+        console.log("Received message:", message);
     });
 
     ws.on('close', () => {
-        console.log("Connection closed");
+        console.log("WebSocket connection closed");
     });
-
-    // Gửi tin nhắn khi kết nối được thiết lập
-    ws.send("Message from server");
 });
 
-// Thiết lập việc ping định kỳ
+// Xử lý HTTP POST request tới /data
+app.post('/data', (req, res) => {
+    console.log('HTTP POST request received at /data:', req.body);
+    if (req.body.script === "moveForward" && connectedWs) {
+        connectedWs.send("moveForward");
+    }
+    res.json({ message: 'Command received and processed' });
+});
+
+// Tạo HTTP server từ Express app
+const server = createServer(app);
+
+// Xử lý yêu cầu nâng cấp từ HTTP sang WebSocket
+server.on('upgrade', (request, socket, head) => {
+    wss.handleUpgrade(request, socket, head, (ws) => {
+        wss.emit('connection', ws, request);
+    });
+});
+
+// Khởi chạy server
+server.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+// Thiết lập việc ping định kỳ để kiểm tra các client WebSocket
 setInterval(() => {
     wss.clients.forEach((ws) => {
         if (!ws.isAlive) return ws.terminate();
-        
+
         ws.isAlive = false;
         ws.ping();
     });
 }, 30000); // Ví dụ ở đây là 30 giây
-
-console.log("WebSocket server is running on port ", PORT);
